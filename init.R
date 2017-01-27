d77fc5b6bd67e309240e7ee2f786e64ad2c9cff5
zawam <jalal-edine.zawam@rte-france.com>
2017-01-27 09:37:15 +0100

    quelques corrections

--- init.R
+++ init.R
@@ -29,15 +29,14 @@ areas[,mean(`MRG. PRICE`),by=area]
 areas[,.(prix_moyen = mean(`MRG. PRICE`)),by=area]
 
 
-# calculer pour chaque area a chauqe tranche horainre le MRG price moyen ====
+# calculer pour chaque area a chaque tranche horaire le MRG prix moyen ====
 
 areas %>% group_by(area,hour) %>% summarise(prix_moyen=mean(`MRG. PRICE`))
 areas[,mean(`MRG. PRICE`),by=.(area,hour)]
 areas[,.(prix_moyen = mean(`MRG. PRICE`)),by=.(area,hour)]
 
 
-
-# calculer pour chaque area a chaque heure la moyenne de tous les maximum ====
+# calculer pour chaque area a chaque heure la moyenne de tous les maximums (exemple de maximum : BALANCE_max) ====
 areas %>% group_by(area,hour) %>% summarise_each(funs(mean),ends_with("max"))
 areas %>% group_by(area,hour) %>% summarise_each(funs(mean),contains("max"))
 areas %>% group_by(area,hour) %>% 
@@ -54,6 +53,8 @@ areas[,lapply(.SD,mean),
 
 areas[,c("area","hour",select_vars(names(areas), contains("max"))),with=FALSE][,lapply(.SD,mean),by=.(area,hour)]
 areas[,c("area","hour",select_vars(names(areas), contains("max"))),with=TRUE]
+
+## effectuer un benchemark sur les traitements précédents
 microbenchmark::microbenchmark(areas[,lapply(.SD,mean),
                                      .SDcols= select_vars(names(areas), contains("max")),
                                      by=.(area,hour)],areas[,c("area","hour",select_vars(names(areas), contains("max"))),with=FALSE][,lapply(.SD,mean),by=.(area,hour)]
@@ -81,34 +82,44 @@ areas2 <- areas %>% filter(area=="a")
 areas2b <- areas[area=="a",]
 
 
-
 # supprimer la colonne area ====
 
 areas3 <- areas2 %>% select(-area)
-areas3b <- areas2b[,area:=NULL]
+areas3b <- areas2b[,area:=NULL]  #la colonne area est aussi supprimé dans areas2b
 
-# ne garder que les colonnes de cout et de prix et d'emission de CO2 ====
+##à voir si vous voulez aborder ce point ici
+is.null(areas3b$area)  #TRUE
+is.null(areas2b$area)  #TRUE
 
-areas4 <- areas3 %>% select(contains("COST"),contains("PRICE"),`CO2 EMIS.`)
+areas2b <- areas[area=="a",]
+areas4b <- copy(areas2b)
+areas4b<-areas4b[,area:=NULL]
+
+is.null(areas4b$area)  #TRUE
+is.null(areas2b$area)  #FALSE
+
+# ne garder que les colonnes de cout, de prix et le cout total de production (ie `OV. COST`) ====
+
+areas4 <- areas3 %>% select(contains("COST"),contains("PRICE"),`OV. COST`)
 
 areas4b <- areas3b[,
-                   select_vars(names(areas3b),contains("COST"),contains("PRICE"),`CO2 EMIS.`),
+                   select_vars(names(areas3b),contains("COST"),contains("PRICE"),`OV. COST`),
                    with=FALSE]
 
-# rajouter une colonne qui correspon au ratio  de CO2 EMIS. par PRICE MRG.====
-# Arrondir le rÃ©sulat a 2 chiffres apres la virgule ====
+# rajouter une colonne qui correspond au ratio de `OV. COST` par PRICE MRG.====
+# Arrondir le resulat a 2 chiffres apres la virgule ====
 
-areas5 <- areas4 %>% mutate(ratio =`CO2 EMIS.`/`MRG. PRICE`,
+areas5 <- areas4 %>% mutate(ratio =`OV. COST`/`MRG. PRICE`,
                             ratio=round(ratio,2))
                             
 
-areas4b[,ratio := round(`CO2 EMIS.`/`MRG. PRICE`,2)]
-areas4b[,ratio := `CO2 EMIS.`/`MRG. PRICE`
+areas4b[,ratio := round(`OV. COST`/`MRG. PRICE`,2)]
+areas4b[,ratio := `OV. COST`/`MRG. PRICE`
         ][,ratio := round(ratio,2)]
 
 
-# changer le nom de la colonne CO2 EMIS. par "emission_co2 ====
-setnames(areas4b,'CO2 EMIS.',"emission_co2")
+# changer le nom de la colonne OV. COST par "cout_total ====
+setnames(areas4b,'OV. COST',"cout_total")
 
 
 # exporter le resultat en CSV====
@@ -117,23 +128,23 @@ setnames(areas4b,'CO2 EMIS.',"emission_co2")
 areas %>% 
   filter(area=="a") %>% 
   select(-area) %>% 
-  select(contains("COST"),contains("PRICE"),`CO2 EMIS.`) %>% 
-  mutate(ratio =`CO2 EMIS.`/`MRG. PRICE`,
+  select(contains("COST"),contains("PRICE"),`OV. COST`) %>% 
+  mutate(ratio =`OV. COST`/`MRG. PRICE`,
         ratio=round(ratio,2)) %>% 
   arrange(ratio) %>%
-  rename(emission_co2 = `CO2 EMIS.`) %>% 
+  rename(cout_total = `OV. COST`) %>% 
   write.csv2(file="out.csv",row.names=FALSE)
 
 
 areas[area=="a",
-      select_vars(names(areas),contains("COST"),contains("PRICE"),`CO2 EMIS.`),
-      with=FALSE][,ratio := round(`CO2 EMIS.`/`MRG. PRICE`,2)][order(ratio)]
+      select_vars(names(areas),contains("COST"),contains("PRICE"),`OV. COST`),
+      with=FALSE][,ratio := round(`OV. COST`/`MRG. PRICE`,2)][order(ratio)]
 
 
 
 browseURL("out.csv")
   
-# changer le nom de la colonne CO2 EMIS. par "emission_co2 ===
+# changer le nom de la colonne OV. COST par "cout_total ====
 
 
 # exporter le rÃ©sultat dans un fichier csv
@@ -151,11 +162,11 @@ browseURL("out.csv")
 areas %>% 
   filter(area=="a") %>% 
   select(-area) %>% 
-  select(contains("COST"),contains("PRICE"),`CO2 EMIS.`) %>% 
-  mutate(ratio =`CO2 EMIS.`/`MRG. PRICE`,
+  select(contains("COST"),contains("PRICE"),`OV. COST`) %>% 
+  mutate(ratio =`OV. COST`/`MRG. PRICE`,
          ratio=round(ratio,2)) %>% 
   arrange(ratio) %>%
-  rename(emission_co2 = `CO2 EMIS.`) %>% 
+  rename(cout_total = `OV. COST`) %>% 
   write.csv2(file="out.csv",row.names=FALSE)
 
 areas
